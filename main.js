95f06b4e5e8073bbc3582276636c30e72f4eddc7
cartudev <113826669+cartudev@users.noreply.github.com>
2023-08-08 10:39:59 -0300

    fixed typo.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 /*
 Un codigo con la idea de resolver el siguiente problema:
-ella nacio en
-Un muchacho se entretiene pintando su tablero de ajedrez. Planea cubrir cada espacio por completo en tonos rojos o azules. Para darle un toque personal, quiere contar con igual cantidad de espacios rojos y azules, evitando a su vez que dos filas o columnas presenten el mismo número de espacios rojos. ¿Podría pintar el tablero siguiendo estos criterios? ¿Qué pasaría si, en lugar de un tablero de ajedrez tradicional de 8x8, tuviera uno enorme de 1000x1000?
 
-Todo está escrito en JS para ser reproducido directamente en una consola, se puede correr en la consola del navegador, o en algún compilador online como: https://www.mycompiler.io/es/new/nodejs
+Un muchacho se entretiene pintando su tablero de ajedrez. Planea cubrir cada espacio por completo en tonos rojos o azules. Para darle 
+un toque personal, quiere contar con igual cantidad de espacios rojos y azules, evitando a su vez que dos filas o columnas presenten el
+mismo número de espacios rojos. ¿Podría pintar el tablero siguiendo estos criterios? ¿Qué pasaría si, en lugar de un tablero de ajedrez
+tradicional de 8x8, tuviera uno enorme de 1000x1000?
+
+Todo está escrito en JS para ser reproducido directamente en una consola, se puede correr en la consola del navegador, o en algún 
+compilador online como: https://www.mycompiler.io/es/new/nodejs
 */
 
 function grid(columns, rows, bColour = 'azul', rColour = 'rojo'){
